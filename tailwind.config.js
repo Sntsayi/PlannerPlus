c311ae10fba0e7dd4ae9509c97cf86d21cd85426
TahaSahraei <taha23sahraii@gmail.com>
2024-03-30 02:24:24 +0430

    write rightSidebar and leftSideBar but not completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-SecondaryColor": "#7A7B7C",
+        "orange-Secondary": "#F97C35",
+        "black-Secondary": "#222325",
+      },
+      spacing: {
+        "30%": "30%",
+        "28%": "28%",
+      },
+    },
+    borderWidth: {
+      DEFAULT: "1px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
+    },
   },
   plugins: [],
 };
